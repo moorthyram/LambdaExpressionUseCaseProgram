ee27cf509def3444123b6bfbd6497412736129c0
Moorthy <moorthy@mobo.co.uk>
2022-08-29 08:07:14 +0530

    UC3_Email

--- RegexC.cs
+++ RegexC.cs
@@ -10,9 +10,10 @@ namespace LambdaExpressionUseCaseProgram
     {
         public static void RegexPattern()
         {
-            String FirstName, LastName;
+            String FirstName, LastName, Email;
 
             Regex nameregex = new Regex("^([A-Z]{1}[a-z]{2}([a-z]+)?)$");
+            Regex emailregex = new Regex("^([0-9a-zA-Z])+([.+-][0-9a-zA-Z]+)?@([0-9a-zA-Z])+([.][a-zA-Z]{2,4})([.][a-zA-Z]{2})?$");
 
 
             Console.WriteLine("Enter the First Name");
@@ -42,6 +43,20 @@ namespace LambdaExpressionUseCaseProgram
             else
             {
                 Console.WriteLine("It is Invalid name, Enter first letter Capital letter");
+            }
+
+            Console.WriteLine("Enter the EmailId");
+            var mail = Console.ReadLine();
+
+            Match matchemail = emailregex.Match(mail);
+            if (matchemail.Success)
+            {
+                Email = mail;
+                Console.WriteLine("It is valid Mail");
+            }
+            else
+            {
+                Console.WriteLine("It is Invalid");
             }
         }
     }
